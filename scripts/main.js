a4c260ce20397802cc774851f8450e60e27dd22b
Kelsey Lemmer <kelseylemmer@gmail.com>
2023-05-12 11:39:22 -0400

    updated ERD, added CSS skeleton, added main.js skeleton, and added index.html skeleton

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+
+const mainContainer = document.querySelector("#container")
+
+const applicationHTML = `
+<h1>CIDER FALLS</h1>
+<article class="details">
+    <section class="detail--column list details__services">
+        <h2>Services</h2>
+        TO DO : !!!! add function
+    </section>
+    <section class="detail--column list details__park areas">
+        <h2>Park Areas</h2>
+        TO DO : !!!! add function
+    </section>
+</article>
+
+<article class="guest names">
+    <h2>GUESTS</h2>
+    <section class="detail--column list details__names">
+        <h2>Guests</h2>
+        TO DO : !!!! add function
+    </section>
+</article>
+`
+
+mainContainer.innerHTML = applicationHTML
